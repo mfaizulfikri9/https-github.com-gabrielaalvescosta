20600128bad5ecddff0511d505c0c53032e50bdc
gabrielacosta <gabrielasilvaalvescosta@gmail.com>
2021-08-14 23:24:59 -0300

    V 2.0 George

--- george/george2.js
+++ george/george2.js
@@ -3,12 +3,12 @@ var mensagem;
     //segunda fase 
     function george2()
     {
-    mensagem = prompt("O que George deve fazer? \n \n 1 - Ser gentil e falar que a chave está embaixo da roupa espacial. \n 2 - Enrolar e dizer que não achou a chave");
+    mensagem = prompt("O que George deve fazer? \n \n 1 - Roubar a roupa, o crachá e se passar por Samuel Bliss  \n 2 - Pegar apenas o crachá e sair de fininho.");
 
     if (mensagem == 1) 
     {
  
-        document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>Os cientistas agradeceram a gentileza de George de avisar que o crachá estava embaixo da roupa espacial. Eles pegaram a chave e foram embora com ela, fechando o laboratório. George sabe que só é possível entrar na nave pelo laboratório. 
+        document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>George tentou pegar a roupa do astronauta e vestir, porém um amigo seu passou pelo corredor e o pegou no flagra. George foi demitido.  
         <a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div>`;
 
     }
@@ -17,11 +17,12 @@ var mensagem;
         document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'>        <div class='cabecalho'>
         <div class='george-cab'></div>
         <h3>Deu certo!</h3>
-    </div>Assim que os cientistas desceram, George pegou a tal chave em suas mãos. Era uma espécie de crachá de identificação de um dos astronautas da tripulação chamado Samuel Bliss. A roupa do astronauta estava em cima da mesa. O que George deve fazer?
-
+    </div>George olhou bem o cartão de Samuel Bliss e sabia que estes cartões eram os que abriam as portas do laboratório secreto. George entrou rapidamente no laboratório e passou o cartão em uma porta que abriu para um elevador secreto que o levava diretamente para a nave. O problema é que havia duas saídas: T01 e T02. T01 era uma saída aberta e com escadas.
+    T02 era uma saída fechada, uma espécie de túnel.
+    
         <ul>
-        <li>1- Pegar apenas o crachá e sair de fininho.</li>
-<li>2 - Roubar a roupa, o crachá e se passar por Samuel Bliss </li>
+        <li>1- T01.</li>
+<li>2 - T02 </li>
 
 </ul>
 <a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/george/george3.html'><button class='botao'>RESPONDER</button></a></div>`;
