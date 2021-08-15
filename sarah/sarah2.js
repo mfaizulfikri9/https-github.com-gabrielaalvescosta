da01f396c9a3d717a8d8de9fd87cce0665a7ff4d
gabrielacosta <gabrielasilvaalvescosta@gmail.com>
2021-08-15 16:31:29 -0300

    adicionado o break (while) js sarah

--- sarah/sarah2.js
+++ sarah/sarah2.js
@@ -11,7 +11,7 @@ var mensagem;
         {
             document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>Sarah começou a desparafusar o boeiro, porém ela não tinha percebido que próximo havia um fio elétrico partido. Ela encostou a chave de fenda e tomou um choque. 
             <a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div>`;
-
+            break;
         }
         else if (mensagem == 2)
         {
