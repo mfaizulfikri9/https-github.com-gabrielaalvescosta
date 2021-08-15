a1b3dc39d44b1f1511b51e4045db1f31ecf952d2
gabrielacosta <gabrielasilvaalvescosta@gmail.com>
2021-08-15 02:36:10 -0300

    Alt. Pagina 2

--- george/george1.js
+++ george/george1.js
@@ -1,10 +1,11 @@
 var mensagem;
 
 //primeira fase 
-while (true) {
 
     function george1()
         {
+        while(true)    
+
         mensagem = prompt("O que George deve fazer? \n \n 1 - Esperar eles irem embora para começar procurar a tal chave.  \n 2 - Perguntar aos cientistas se precisam de ajuda.");
 
     if (mensagem == 1) 
@@ -12,7 +13,7 @@ while (true) {
             document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>Os cientistas ficaram um bom tempo conversando e George aguardando ansiosamente o momento de entrar na sala. Assim que deu o horário do almoço, os cientistas organizaram a mesa e acharam a bendita chave. Levaram a chave embora. George perdeu a chance de entrar na nave.
 
             <a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div></div>`;
-            break;
+        
         }
     else if (mensagem == 2)
         {
@@ -22,4 +23,3 @@ while (true) {
         }
 
     }
-}
