0f3a896c367346ad4caaf3a67e60ac5521ee0e0f
gabrielacosta <gabrielasilvaalvescosta@gmail.com>
2021-08-15 13:26:00 -0300

    Versão com o While adicionada

--- george/george2.js
+++ george/george2.js
@@ -1,38 +1,28 @@
 var mensagem;
 
-    //segunda fase 
-    function george2()
-    {
-    mensagem = prompt("O que George deve fazer? \n \n 1 - Roubar a roupa, o crachá e se passar por Samuel Bliss  \n 2 - Pegar apenas o crachá e sair de fininho.");
+//segunda fase 
+        function george2() {
 
-    if (mensagem == 1) 
-    {
- 
-        document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>George tentou pegar a roupa do astronauta e vestir, porém um amigo seu passou pelo corredor e o pegou no flagra. George foi demitido.  
-        <a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div>`;
+        while(mensagem != 1 || mensagem != 2) {
 
-    }
-    else if (mensagem == 2)
-    { 
-        document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'>        <div class='cabecalho'>
-        <div class='george-cab'></div>
-        <h3>Deu certo!</h3>
-    </div>George olhou bem o cartão de Samuel Bliss e sabia que estes cartões eram os que abriam as portas do laboratório secreto. George entrou rapidamente no laboratório e passou o cartão em uma porta que abriu para um elevador secreto que o levava diretamente para a nave. O problema é que havia duas saídas: T01 e T02. T01 era uma saída aberta e com escadas.
-    T02 era uma saída fechada, uma espécie de túnel.
-    
-        <ul>
-        <li>1- T01.</li>
-<li>2 - T02 </li>
+        mensagem = prompt("O que George deve fazer? \n \n 1 - Ser gentil e falar que a chave está embaixo da roupa espacial.   \n 2 - Enrolar e dizer que não achou a chave.");
 
-</ul>
-<a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/george/george3.html'><button class='botao'>RESPONDER</button></a></div>`;
-    }
-    else 
-    {   
-        
-        alert(`Digite 1 ou 2`);
-        return george2();
+        if (mensagem == 1) 
+        {
+            document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>Os cientistas ficaram um bom tempo conversando e George aguardando ansiosamente o momento de entrar na sala. Assim que deu o horário do almoço, os cientistas organizaram a mesa e acharam a bendita chave. Levaram a chave embora. George perdeu a chance de entrar na nave.<a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div></div>`;
+            break;
+        }
+        else if (mensagem == 2)
+        {
+
+            location.replace("./george3.html");
+            break;
+        }
+        else 
+        {
+            alert(`Digite 1 ou 2`);
+            break;
+        }
     }
 }
 
-george2();
--- george/george4.js
+++ george/george4.js
@@ -1,33 +1,29 @@
 var mensagem;
 
+//primeira fase 
+        function george4()
+        {
 
+            while(mensagem != 1 || mensagem != 2) {
 
-    //quarta fase 
-    function george4()
-    {
-    mensagem = prompt("Qual caminho George deve seguir? \n \n 1 - T01, uma saída aberta e com escadas.  \n 2 - T02, uma saída fechada, uma espécie de túnel.");
+        mensagem = prompt("O que George deve fazer? \n \n 1 - T01   \n  2 - T02");
 
     if (mensagem == 1) 
-    {
-        document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>YOU <h2 class='tada'>WIN</h2></h2></div>
-        George conseguiu subir as escadas e foi parar dentro da nave. Ele entrou de fininho, mas percebeu que não tinha ninguém lá dentro. Agora George vai para a missão espacial!
-s
-        <a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div></div>`;
-
-    }
+        {
+            document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>Os cientistas ficaram um bom tempo conversando e George aguardando ansiosamente o momento de entrar na sala. Assim que deu o horário do almoço, os cientistas organizaram a mesa e acharam a bendita chave. Levaram a chave embora. George perdeu a chance de entrar na nave.<a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div></div>`;
+            break;
+        }
     else if (mensagem == 2)
-    {
+        {
 
-        document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>Parecia uma boa ideia ir pela T02, mas ele infelizmente encontrou um dos tripulantes conversando no meio do caminho. Ele foi descoberto. 
-
-        <a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div>`;
-    }
+            location.replace("./george5.html");
+            break;
+        }
     else 
-    {
-        alert(`Digite 1 ou 2`);
-        return george4();
-
+        {
+            alert(`Digite 1 ou 2`);
+            break;
+        }
     }
 }
 
-george4();
