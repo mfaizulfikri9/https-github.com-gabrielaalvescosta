361365b115d66f32d55277631c958940bc3c409b
Gabriela Costa <87823944+gabrielaalvescosta@users.noreply.github.com>
2021-08-16 14:44:38 -0300

    posicoes corrigidas

--- sarah/sarah4.js
+++ sarah/sarah4.js
@@ -9,12 +9,12 @@ var mensagem;
 
         if (mensagem == 1) 
         {
-            document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>Martha chegou nos soldados e disse "está um belo dia hoje, não?". Ficou puxando papo com os soldados até que um percebeu que ela estava mesmo era tentando tirar atenção deles. Eles a expulsaram da região e chamaram a polícia.<a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div></div>`;
+            location.replace("https://gabrielaalvescosta.github.io/ProjetoModulo1/sarah/sarah5.html");
             break;
         }
         else if (mensagem == 2)
         {
-            location.replace("https://gabrielaalvescosta.github.io/ProjetoModulo1/sarah/sarah5.html");
+            document.getElementsByClassName('content')[0].innerHTML=`<div class='popup'><div class='gameover-texto'><div class='gameover'><h2 class='tada'>GAME <h2 class='tada'>OVER</h2></h2></div>Martha chegou nos soldados e disse "está um belo dia hoje, não?". Ficou puxando papo com os soldados até que um percebeu que ela estava mesmo era tentando tirar atenção deles. Eles a expulsaram da região e chamaram a polícia.<a href='https://gabrielaalvescosta.github.io/ProjetoModulo1/index.html'><button class='botao-gameover'>RECOMEÇAR O JOGO</button></a></div></div></div>`;
             break;
         }
         else 
